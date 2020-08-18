a02fa06c3f905a34b8468c11a5fffe186129a82e
kledenai <bruno.silva1643@etec.sp.gov.br>
2020-08-18 06:01:14 -0300

    adding routes example

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -1,9 +1,29 @@
 module.exports = {
-  async index(req, res) {
-    return res.json({ message: 'Hello World' })
+  async index(request, response) {
+    return response.json([
+      'project 1'
+    ])
   },
 
-  async store(req, res) {
+  async store(request, response) {
+    return response.json([
+      'Project 1',
+      'Project 2'
+    ])
+  },
 
+  async update(request, response) {
+    return response.json([
+      'Project 4',
+      'Project 2',
+      'Project 3'
+    ])
+  },
+
+  async delete(request, response) {
+    return response.json([
+      'Project 2',
+      'Project 3'
+    ])
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const ProjectController = require('./controllers/ProjectController');
 const routes = express.Router();
 
 routes.get('/projects', ProjectController.index);
+routes.post('/projects/create', ProjectController.store);
+routes.put('/projects/update/:id', ProjectController.update);
+routes.delete('/projects/delete/:id', ProjectController.delete);
 
 module.exports = routes;

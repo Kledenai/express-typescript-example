105e031d34997aad3d4893639335a15d81dde4bf
kledenai <bruno.silva1643@etec.sp.gov.br>
2020-08-18 06:13:21 -0300

    adding new route and controllers

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -1,15 +1,19 @@
 module.exports = {
-  async index(request, response) {
+  async index (request, response) {
     return response.json([
       'project 1'
     ])
   },
 
-  async store(request, response) {
+  async store (request, response) {
     return response.json([
       'Project 1',
       'Project 2'
     ])
+  },
+
+  async show (request, response) {
+    return response.json({ project: 'Project 1' })
   },
 
   async update(request, response) {
@@ -20,7 +24,7 @@ module.exports = {
     ])
   },
 
-  async delete(request, response) {
+  async delete (request, response) {
     return response.json([
       'Project 2',
       'Project 3'
